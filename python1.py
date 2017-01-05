41f6aad1c129c68c70a1281f0b4e1fcf2dee75fb
Liusangedian <sangediandiandian@gmail.com>
2017-01-05 21:10:26 +0800

    hello git

---
+++ python1.py
@@ -0,0 +1,26 @@
+import turtle
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    brad = turtle.Turtle()
+    brad.shape("square")
+    brad.color("yellow")
+    brad.speed(2)
+
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+	
+	brad.circle(100)
+	
+    window.exitnoclick()
+    
+    
+draw_square()
